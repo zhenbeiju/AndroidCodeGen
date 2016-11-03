c0607fd9a02947ba1d0c2155b26278f8ff770cb4
zhanglin <zhanglin01@wywk.cn>
2016-11-03 20:02:15 +0800

    support args

--- src/Main.java
+++ src/Main.java
@@ -25,23 +25,22 @@ public class Main {
 
     private static String packageName = null;
     private static String modelName = null;
+    private static List<FieldModel> fieldModels = new ArrayList<>();
 
     public static void main(String[] args) {
         ParseQuene<String> parseQuene = new ParseQuene<String>();
         for (String s : args) {
             parseQuene.add(s);
         }
-        if (parseArgs(parseQuene)) {
-
+        if (!parseArgs(parseQuene)) {
             return;
         }
 
         if (init()) {
-            List<FieldModel> testModel = new ArrayList<>();
-            testModel.add(new FieldModel("Name", FieldType.STRING));
-            testModel.add(new FieldModel("Age", FieldType.INT));
-            testModel.add(new FieldModel("Price", FieldType.DOUBLE));
-            CodeGen codeGen = new CodeGen("Test", testModel);
+            if(checkParam(packageName)){
+                KeyList.packageName = packageName;
+            }
+            CodeGen codeGen = new CodeGen(modelName, fieldModels);
             codeGen.doGenerate();
             System.err.print(DEPEND_NOTICE);
         }
@@ -49,6 +48,7 @@ public class Main {
 
 
     private static boolean init() {
+
         String path = FileUtil.getPath();
         System.out.println(path);
         KeyList.res_path = path.substring(0, path.lastIndexOf("java")) + "res" + File.separator;
@@ -68,35 +68,79 @@ public class Main {
     private static boolean parseArgs(ParseQuene<String> parseQuene) {
         String arg0 = parseQuene.pop();
         if (arg0 == null) {
-            return false;
+            if (!checkParam(modelName)) {
+                System.err.println("you need assign a model name , use: -g <modelName> ");
+                return false;
+            }
+            if (fieldModels.size() == 0) {
+                System.err.println("you need assign a field , use: -attr <fieldName> <fieldType>");
+                return false;
+            }
+
+            return true;
         }
         switch (arg0) {
             case "-help":
             case "-h":
             case "usage":
                 System.out.println(HELP);
-                return true;
+                return false;
             case "-g":
             case "-G":
                 modelName = parseQuene.pop();
+                if (checkParam(modelName)) {
+                    return parseArgs(parseQuene);
+                } else {
+                    System.err.println("error model name");
+                    return false;
+                }
 
-                break;
             case "-p":
                 packageName = parseQuene.pop();
-                break;
-            case "-db":
+                if (checkParam(packageName)) {
+                    return parseArgs(parseQuene);
+                } else {
+                    System.err.println("error package name");
+                    return false;
+                }
 
+            case "-db":
                 break;
             case "-api":
                 break;
             case "-attr":
-                break;
+                List<String> attrs = new ArrayList<>();
+                while (parseQuene.touch() != null && !parseQuene.touch().startsWith("-")) {
+                    attrs.add(parseQuene.pop());
+                }
+                try {
+                    for (int i = 0; i < attrs.size(); i += 2) {
+                        String name = attrs.get(i);
+                        String type = attrs.get(i+1);
+                        try {
+                            FieldType type1 = FieldType.valueOf(type.toUpperCase());
+                            FieldModel model = new FieldModel(name, type1);
+                            fieldModels.add(model);
+                        }catch (Exception e){
+                            System.err.println(" fieldType not support :\n support type is: int, string, float, double, name, email, password");
+                            return false;
+                        }
+                    }
+
+
+                    return parseArgs(parseQuene);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.err.println("error attr values, format is :  <fieldName>  <fieldType>");
+                    return false;
+                }
+
             default:
-                System.err.println("unkown argment");
-                return true;
+                System.err.println("unknown arg");
+                return false;
         }
 
-        return false;
+        return true;
     }
 
 
@@ -132,8 +176,12 @@ public class Main {
                 return null;
             }
         }
-
     }
 
-
+    private static boolean checkParam(String param) {
+        if (param != null && param.length() > 0) {
+            return true;
+        }
+        return false;
+    }
 }
